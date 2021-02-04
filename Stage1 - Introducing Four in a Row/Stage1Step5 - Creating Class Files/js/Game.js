18f526a5e9db2daadeaaccc176fca34c8ab76219
AndrewSimons3 <simdrew07@gmail.com>
2021-02-03 21:27:23 -0600

    createPlayers method created in Game class

--- Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Game.js
+++ Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Game.js
@@ -1,1 +1,19 @@
-class Game {}
+class Game {
+  constructor() {
+    this.board = new Board;
+    this.ready = false;
+    this.players = createPlayers()
+  }
+
+  /** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+
+  createPlayers() {
+    const players = [new Player('Player 1', 1, '#e15258', true),
+                     new Player('Player 2', 2, '#e59a13')];
+
+    return players;
+  }
+}
